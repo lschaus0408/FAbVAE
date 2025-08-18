960b3ac6504c69a595b07f60df44119c6c8676a2
lschaus0408 <schauslucas@hotmail.com>
2025-08-17 17:42:44 -0700

    Initial commit for training config parser

---
+++ fabvae/cli/training_config_parser.py
@@ -0,0 +1,9 @@
+"""
+--------------------    FAbVAE     ----------------------
+FAbVAE is a ...
+Author: Lucas Schaus
+-------------- Training Config Parser -------------------
+Parser for training config files. Defines what data to
+load and in what format, as well as what model to train
+and with which hyperparameters and architecture.
+"""
