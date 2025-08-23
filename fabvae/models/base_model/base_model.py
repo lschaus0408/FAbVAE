9c1fc0ba7b9186b77a77aaa9d14062a8cf985ca9
lschaus0408 <schauslucas@hotmail.com>
2025-08-22 18:32:01 -0700

    Added Abstract Model Class for Training Script

---
+++ fabvae/models/base_model/base_model.py
@@ -0,0 +1,41 @@
+"""
+--------------------    FAbVAE     ----------------------
+FAbVAE is a ...
+Author: Lucas Schaus
+-------------------  Base Model ABC ---------------------
+Abstracts the Base Model.
+"""
+
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import torch
+import torch.nn as nn
+
+from fabvae.models.base_model.encoder import AbVAEEncoder
+from fabvae.models.base_model.decoder import AbVAEDecoder
+
+
+class FAbVAEBase(ABC, nn.Module):
+    """
+    ## Abstract Class of a FAbVAE Model
+    """
+
+    encoder: AbVAEEncoder
+    decoder: AbVAEDecoder
+
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def elbo(
+        self,
+        input_embedding: torch.Tensor,
+        position_weights: Optional[torch.Tensor] = None,
+        eps: float = 1e-8,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        ## Abstract Method For Loss Calculation
+        """
